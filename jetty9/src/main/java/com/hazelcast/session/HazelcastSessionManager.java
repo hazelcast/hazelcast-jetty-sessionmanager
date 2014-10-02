30b1885c848761ec968629b451f5dca08acc60a5
emrahkocaman <emrahkocaman@gmail.com>
2014-10-02 16:23:15 +0300

    Jetty session replication initial commit

--- jetty9/src/main/java/com/hazelcast/session/HazelcastSessionManager.java
+++ jetty9/src/main/java/com/hazelcast/session/HazelcastSessionManager.java
@@ -112,7 +112,6 @@ public class HazelcastSessionManager extends NoSqlSessionManager {
         } catch (Exception e) {
             LOG.warn("HazelcastSessionManager:save:exception", e);
         }
-
         return null;
     }
 

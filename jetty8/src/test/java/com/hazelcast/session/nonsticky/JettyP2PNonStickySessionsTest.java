f2a2871736d22515de051a5e4258fa68e24410b9
emrahkocaman <emrahkocaman@gmail.com>
2014-10-10 14:24:20 +0300

    jetty9 support

---
+++ jetty8/src/test/java/com/hazelcast/session/nonsticky/JettyP2PNonStickySessionsTest.java
@@ -0,0 +1,12 @@
+package com.hazelcast.session.nonsticky;
+
+import com.hazelcast.session.JettyConfigurator;
+import com.hazelcast.session.WebContainerConfigurator;
+
+public class JettyP2PNonStickySessionsTest extends P2PNonStickySessionsTest {
+
+    @Override
+    protected WebContainerConfigurator<?> getWebContainerConfigurator() {
+        return new JettyConfigurator();
+    }
+}

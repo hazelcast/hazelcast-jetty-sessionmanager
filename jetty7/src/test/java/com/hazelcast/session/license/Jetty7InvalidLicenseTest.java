254998c8deed6a559ae5037e2b0a138f7e340341
ibrahim gürses <ibrahim@hazelcast.com>
2015-03-31 11:12:34 +0300

    Moved exception methods to abstract test methods, added JDK 6 ignore to Jetty9 tests.

--- jetty7/src/test/java/com/hazelcast/session/license/Jetty7InvalidLicenseTest.java
+++ jetty7/src/test/java/com/hazelcast/session/license/Jetty7InvalidLicenseTest.java
@@ -15,23 +15,4 @@ public class Jetty7InvalidLicenseTest extends AbstractInvalidLicenseTest {
         return new JettyConfigurator("hazelcast-without-license.xml","hazelcast-client-without-license.xml");
     }
 
-    @Override
-    protected Matcher<? extends Throwable> getCause() {
-        return new BaseMatcher<Throwable>() {
-            @Override
-            public boolean matches(Object o) {
-                return o == null;
-            }
-
-            @Override
-            public void describeTo(Description description) {
-
-            }
-        };
-    }
-
-    @Override
-    protected Class<? extends Throwable> getException() {
-        return InvalidLicenseException.class;
-    }
 }
